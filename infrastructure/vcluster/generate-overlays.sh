9d62b9ec352d9b8611567fb9d563a97a97245767
Felix Kampfer <felix.kampfer@qaware.de>
2025-10-27 17:45:36 +0100

    fix: create namespace for each vcluster

--- infrastructure/vcluster/generate-overlays.sh
+++ infrastructure/vcluster/generate-overlays.sh
@@ -23,17 +23,30 @@ do
   OVERLAY_DIR="overlays/$VCLUSTER_NAME"
   mkdir -p $OVERLAY_DIR
 
+  # Create the namespace resource
+  cat <<EOF > $OVERLAY_DIR/namespace.yaml
+apiVersion: v1
+kind: Namespace
+metadata:
+  name: $VCLUSTER_NAME
+EOF
+
   # Create the kustomization.yaml for the overlay
   cat <<EOF > $OVERLAY_DIR/kustomization.yaml
 apiVersion: kustomize.config.k8s.io/v1beta1
 kind: Kustomization
+namespace: $VCLUSTER_NAME
 resources:
+  - namespace.yaml
   - ../../base
 
 patches:
   - patch: |-
       - op: replace
         path: /metadata/name
+        value: $VCLUSTER_NAME
+      - op: replace
+        path: /metadata/namespace
         value: $VCLUSTER_NAME
     target:
       kind: HelmRelease
